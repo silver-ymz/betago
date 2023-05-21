ee6f94da42dd6c9a17f60105b0b09d15dc72f882
silver-ymz <yinmingzhuo@gmail.com>
2023-05-21 23:13:01 +0800

    complete homework 2
    Signed-off-by: silver-ymz <yinmingzhuo@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,20 +1,34 @@
 import sys
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+from PySide6.QtGui import QColorConstants
+from setting import load_word
 
 
 class BoardWidget(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, init_table: list[list[int]] = None):
         super().__init__()
 
-        self.board = [[0 for _ in range(4)] for _ in range(4)]
+        self.board_size = 16
+        self.cell_size = 50
+        if init_table is None:
+            self.board = [[0 for _ in range(self.board_size)]
+                          for _ in range(self.board_size)]
+        else:
+            assert len(init_table) == self.board_size
+            assert all(len(i) == self.board_size for i in init_table)
+            self.board = init_table
 
-        self.table = QtWidgets.QTableWidget(4, 4)
+        self.table = QtWidgets.QTableWidget(self.board_size, self.board_size)
         self.table.horizontalHeader().hide()
         self.table.verticalHeader().hide()
 
-        for i in range(4):
-            self.table.setColumnWidth(i, 50)
-            self.table.setRowHeight(i, 50)
+        for i in range(self.board_size):
+            self.table.setColumnWidth(i, self.cell_size)
+            self.table.setRowHeight(i, self.cell_size)
+
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                self.table.setItem(i, j, QtWidgets.QTableWidgetItem())
 
         self.table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
@@ -28,21 +42,25 @@ class BoardWidget(QtWidgets.QWidget):
         self.update_board()
 
     def update_board(self):
-        for i in range(4):
-            for j in range(4):
-                self.table.setItem(
-                    i, j, QtWidgets.QTableWidgetItem(str(self.board[i][j])))
+        for i in range(16):
+            for j in range(16):
+                if self.board[i][j] == 0:
+                    self.table.item(i, j).setBackground(QColorConstants.White)
+                else:
+                    self.table.item(i, j).setBackground(QColorConstants.Black)
 
-    def cell_clicked(self, row, column):
+    @QtCore.Slot(int, int)
+    def cell_clicked(self, row: int, column: int):
         self.board[row][column] = 1 - self.board[row][column]
         self.update_board()
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
+    word = load_word('settings/1.txt')
 
-    widget = BoardWidget()
-    widget.resize(300, 300)
+    widget = BoardWidget(word)
+    widget.resize(1000, 1000)
     widget.show()
 
     sys.exit(app.exec())
